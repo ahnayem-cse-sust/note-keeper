e17ad8268af204b250ff99e085edd52758411c18
Nayem <ahnayem.sust@gmail.com>
2020-03-12 23:42:15 +0600

    detail edit

--- note_keeper/lib/screens/note_detail.dart
+++ note_keeper/lib/screens/note_detail.dart
@@ -9,4 +9,90 @@ class NoteDetail extends StatefulWidget {
   }
 }
 
-class
+class NoteDetailState extends State<NoteDetail> {
+
+  static var _priorities = ['High', 'Low'];
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Note'),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+
+            ListTile(
+              title: DropdownButton(
+                  items: _priorities.map((String dropDownStringItem){
+                    return DropdownMenuItem<String> (
+                      value: dropDownStringItem,
+                      child: Text(dropDownStringItem),
+                    );
+                  }).toList(),
+
+                  style: textStyle,
+
+                  value: 'Low',
+
+                  onChanged: (valueSelectedByUser){
+                    setState(() {
+                      debugPrint('User selected $valueSelectedByUser');
+                    });
+                  }
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextField(
+                controller: titleController,
+                style: textStyle,
+                onChanged: (value){
+                  debugPrint('Something printed');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextField(
+                controller: titleController,
+                style: textStyle,
+                onChanged: (value){
+                  debugPrint('Something printed');
+                },
+                decoration: InputDecoration(
+                    labelText: 'Description',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+
+}
