297f165494372dae864e25669c59f616bac6a9a7
Nayem <ahnayem.sust@gmail.com>
2020-03-13 20:42:54 +0600

    insert db

--- note_keeper/lib/utils/database_helper.dart
+++ note_keeper/lib/utils/database_helper.dart
@@ -5,7 +5,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:notekeeper/models/note.dart';
 
 class DatabaseHelper {
-
   static DatabaseHelper _databaseHelper;
   static Database _database;
 
@@ -25,17 +24,43 @@ class DatabaseHelper {
     return _databaseHelper;
   }
 
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initializeDatabase();
+    }
+    return _database;
+  }
+
   initializeDatabase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String path = directory.path + 'notes.db';
-    
-    var notesDatabase = await openDatabase(path, version: 1,onCreate: _createDb);
+
+    var notesDatabase =
+        await openDatabase(path, version: 1, onCreate: _createDb);
     return notesDatabase;
   }
 
   void _createDb(Database db, int newVersion) async {
-
-    await db.execute('CREATE TABLE $noteTable($colId INTERGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
+    await db.execute(
+        'CREATE TABLE $noteTable($colId INTERGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
         '$colDescription TEXT, $colPriority INTERGER, $colDate TEXT)');
   }
+
+  // Fetch Operation: Get all note objects from database
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+
+//    var result = await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    var result = await db.query(noteTable, orderBy: '$colPriority ASC');
+    return result;
+  }
+
+  // Insert Operation: Insert a Note object database
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  // Update Operation:
 }
