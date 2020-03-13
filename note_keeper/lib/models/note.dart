af060fc6f7da34915b4a91c1a8af63c0cb25892c
Nayem <ahnayem.sust@gmail.com>
2020-03-13 18:51:26 +0600

    note

---
+++ note_keeper/lib/models/note.dart
@@ -0,0 +1,20 @@
+
+class Note {
+
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  Note.withId(this._id, this._title, this._date, this._priority, [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  int get priority => _priority;
+  String get date => _date;
+
+}
